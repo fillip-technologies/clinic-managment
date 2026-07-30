91f34c7718c11719852dc3c62160d9e621133429
Fillip Dev <developer@filliptechnologies.com>
2026-07-30 15:21:10 +0530

    Update sidebar.blade.php

--- resources/views/admin/loyout/sidebar.blade.php
+++ resources/views/admin/loyout/sidebar.blade.php
@@ -47,7 +47,6 @@
 
             <!-- Patients -->
             <a href="{{ route('list.patient') }}"
-            
                 class="sidebar-item flex items-center gap-4 px-4 py-3 rounded-lg text-slate-300 hover:bg-slate-700 hover:text-white transition">
                 <i class="fas fa-hospital-user w-5"></i>
                 <span class="font-medium">Patients</span>
