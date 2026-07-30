fbffe52b49e998f7344ac058e43b1835dee77e85
Fillip Dev <developer@filliptechnologies.com>
2026-07-30 17:25:02 +0530

    Update edit.blade.php

--- resources/views/admin/patients/edit.blade.php
+++ resources/views/admin/patients/edit.blade.php
@@ -219,12 +219,6 @@
                                placeholder="+91 98765 43210"
                                class="w-full rounded-xl border border-[#d3dfea] px-4 py-2.5 text-sm bg-[#fafdff] input-focus">
                     </div>
-
-
-
-
-
-                    
                     <!-- New Registration No. -->
                     <div class="col-span-1">
                         <label class="block text-xs font-semibold uppercase tracking-wide text-[#2f5a77] mb-1">
