dec90c5685bb9ff3876a6a4e808d40bd11f9c342
Fillip Dev <developer@filliptechnologies.com>
2026-07-31 15:48:51 +0530

    Update PatientClinicalRecord.php

--- app/Models/PatientClinicalRecord.php
+++ app/Models/PatientClinicalRecord.php
@@ -3,9 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class PatientClinicalRecord extends Model
 {
+    use SoftDeletes;
+    
       protected $table = 'patient_clinical_records';
       protected $fillable = [
         'patient_id',
